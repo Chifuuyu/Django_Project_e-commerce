2dd729f3fccc7a368be7f7b80186597e205067ba
paulr <pragsac11@gmail.com>
2022-07-19 00:03:48 +0800

    settings.py templates updated

--- Ecommerce_Project/settings.py
+++ Ecommerce_Project/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'Ecommerce_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'shopApp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
